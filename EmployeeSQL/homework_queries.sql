33ec41fe5ebf39c49169b73bb713f3f570e76283
Felipe Lopez <you@example.commr.felipelopez@gmail.com>
2020-10-17 01:30:06 +0000

    finished hw

--- EmployeeSQL/homework_queries.sql
+++ EmployeeSQL/homework_queries.sql
@@ -68,3 +68,10 @@ SELECT last_name, COUNT(emp_no)
 FROM employees
 GROUP BY last_name
 ORDER BY count desc;
+
+--Epilogue
+--Evidence in hand, you march into your boss's office and present the visualization. With a sly grin, your boss thanks you for your work. On your way out of the office, you hear the words, "Search your ID number." You look down at your badge to see that your employee ID number is 499942.
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = '499942';
